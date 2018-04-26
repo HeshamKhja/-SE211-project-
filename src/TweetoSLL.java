aa9bc4b3693e6f9a2e8a6731ab4bf8ab12280a2a
HeshamKhja <215110368@psu.edu.sa>
2018-04-26 11:01:37 +0300

    Update TweetoSLL.java

--- src/TweetoSLL.java
+++ src/TweetoSLL.java
@@ -1,61 +1,80 @@
+package se211project;
+
 /**
  * Created by abdll on 12/21/2017.
  */
 public class TweetoSLL {
-    public Tweeto head;
-    public int size;
-    private int count;
+	
+    public Tweeto head;//Marks the head of the Singly Linked List
+    public int size;//Keeps track of the size of the Singly Linked List
+    private int numOfMatchedTweets;//numOfMatchedTweetss the number of times the searched string was found.
+	
     public TweetoSLL() {}
+	
     public Tweeto getHead() {
         return head;
     }
+	
     public void setHead(Tweeto head) {
         this.head = head;
     }
+	
     public int getSize() {
         return size;
     }
+	
     public void setSize(int size) {
         this.size = size;
     }
-    public int getCount() {
-        return count;
+	
+    public int getNumOfMatchedTweets() {
+        return numOfMatchedTweets;
     }
-    public void setCount(int count) {
-        this.count = count;
+	
+    public void setNumOfMatchedTweets(int numOfMatchedTweets) {
+        this.numOfMatchedTweets = numOfMatchedTweets;
     }
-    public Tweeto Search(String U, String K) {
+	
+    public Tweeto Search(String User, String Keyword) {
         Tweeto temp = head;
+	//Iterating through the SLL to find matching results.
         while (temp != null) {
-            if (temp.getTweet().toLowerCase().contains(K.toLowerCase()) && temp.getID().toLowerCase().startsWith(U.toLowerCase()))
-                count++;
+            //Checks if the searched string is included in the tweet & if the Tweet user's ID starts with the specified letter
+            if (temp.getTweet().toLowerCase().contains(Keyword.toLowerCase()) && temp.getID().toLowerCase().startsWith(User.toLowerCase()))
+                numOfMatchedTweets++;
             temp = temp.getNext();
         }
         return temp;
     }
-    public void insert(Tweeto t) {
+
+    //Inserts a tweet into the Singly Linked List
+    public void insert(Tweeto tweet) {
+        //if SLL is empty it adds the tweet as head and returns
         if (head == null) {
-            head = t;
-            t.setNext(null);
+            head = tweet;
+            tweet.setNext(null);
             size++;
             return;
         }
-//add the new node N to the head of the list
-        t.setNext(head);
-        head = t;
+        //if it is not empty, it adds the the tweet to the beginning of the list as a head
+        tweet.setNext(head);
+        head = tweet;
         size++;
         return;
     }
+	
     @Override
     public String toString() {
-        String out = "";
+        String totalTweets = "";
         Tweeto temp = head;
+        //iterates through the SLL adding each tweet to the string totalTweets
         while (temp != null) {
-            out = out + temp;
+            totalTweets = totalTweets + temp;
             temp = temp.getNext();
         }
+        //adds an empty line after collected tweets and doesn't add a line if list is empty
         if (head != null)
-            out += "\n";
-        return out;
+            totalTweets += "\n";
+        return totalTweets;
     }
 }
